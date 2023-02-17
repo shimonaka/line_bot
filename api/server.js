c6216560aa37c61ed3b87826ef4fb3fcd28b6cf6
“katsumasa <shimonaka@uriup.com>
2023-02-17 09:35:01 +0900

    疎通確認変更

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 	//ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
 	if (req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff') {
-		res.send('Hello LINE BOT!(POST)');
+		res.send('LINE BOT!(POST)');
 		console.log('疎通確認用');
 		return;
 	}
